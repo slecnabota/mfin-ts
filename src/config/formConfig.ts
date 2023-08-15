39d028166c771d623e65577eb337f03db64e7681
Akbota <akbotamold@gmail.com>
2023-08-15 16:51:15 +0600

    typescript

--- src/config/formConfig.ts
+++ src/config/formConfig.ts
@@ -1,31 +1,8 @@
-// import get from 'lodash/get'
 
-// const messages = {
-//   min: 'нужно больше',
-//   max: 'нужно меньше',
-//   email: 'Указанный адрес электронной почты некорректен',
-//   regexp: 'Поле имеет неверный формат',
-//   equals: 'Поля не совпадают',
-//   mask: 'Введите все символы маски',
-//   required: 'Заполните поле'
-// };
-
-// const input = (name, self) => {
-//   const modelValue = self.get(name);
-//   const maskedValue = self.getMask(name) ? self.applyMask(name, modelValue) : modelValue; 
-//   return {
-//       modelValue: maskedValue,
-//       'onUpdate:modelValue': (v) => self.set(name, v),
-//       error: get(self.getStore().formErrors, self.formName+'.'+name+'.0'),
-//   };
-// };
-
-// export default {
-//   messages, input
-// };
 import get from 'lodash/get';
 
 interface Messages {
+  [key: string]: string;
   min: string;
   max: string;
   email: string;
--- src/validation/validations.ts
+++ src/validation/validations.ts
@@ -1,101 +1,60 @@
-// const min = (values, name, param) => {
-//   const value = values[name]
-//   if (value.length > 0 && value.length <= param) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
+type FormValues = Record<string, string>;
 
-// const max = (values, name, param) => {
-//   const value = values[name]
-//   if (value.length >= param) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-// const email = (values, name, param) => {
-//   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//   return regexp(values, name, emailPattern)
-// }
-
-// const regexp = (values, name, param) => {
-//   const value = values[name]
-//   const patternRegex = new RegExp(param);
-//   return patternRegex.test(value)
-// }
-
-// const required = (values, name, param) => {
-//   const value = values[name]
-//   if (value.length > 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-// const maskFilled = (values, name, param) => {
-//   const value = values;
-//   const mask = param;
-
-//   const maskedValue = mask.replace(/#/g, '_');
-//   const sanitizedValue = value.replace(/_/g, '');
-//   if (sanitizedValue.length !== maskedValue.length) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-// };
-// export {
-//   min, max, required, email, regexp, maskFilled
-// };
-const min = (values: any, name: string, param: string): boolean => {
-  const value = values[name];
-  if (value.length > 0 && value.length <= parseInt(param)) {
-    return false;
-  } else {
-    return true;
-  }
+const min = (values: FormValues, name: string, param: string): boolean => {
+    const value = values[name];
+    if (value.length > 0 && value.length <= parseInt(param)) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
-const max = (values: any, name: string, param: string): boolean => {
-  const value = values[name];
-  if (value.length >= parseInt(param)) {
-    return false;
-  } else {
-    return true;
-  }
+const max = (values: FormValues, name: string, param: any): boolean => {
+    const value = values[name];
+    if (value.length >= parseInt(param)) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
-const email = (values: any, name: string, param: string): boolean => {
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regexp(values, name, emailPattern);
+const email = (values: FormValues, name: string, param: any): boolean => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexp(values, name, emailPattern);
 };
 
-const regexp = (values: any, name: string, param: RegExp): boolean => {
-  const value = values[name];
-  return param.test(value);
+const regexp = (values: FormValues, name: string, param: RegExp): boolean => {
+    const value = values[name];
+    return param.test(value);
 };
 
-const required = (values: any, name: string, param: string): boolean => {
-  const value = values[name];
-  return value.length > 0;
+const required = (values: FormValues, name: string, param: any): boolean => {
+    const value = values[name];
+    if (value.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
-const maskFilled = (values: string, name: string, param: string): boolean => {
-  const value = values;
-  const mask = param;
+const maskFilled = (values: string, name: string, param: any): boolean => {
+    const value = values;
+    const mask = param;
 
-  const maskedValue = mask.replace(/#/g, '_');
-  const sanitizedValue = value.replace(/_/g, '');
-  return sanitizedValue.length === maskedValue.length;
+    const maskedValue = mask.replace(/#/g, '_');
+    const sanitizedValue = value.replace(/_/g, '');
+    if (sanitizedValue.length !== maskedValue.length) {
+        return false;
+    } else {
+        return true;
+    }
 };
 
 export {
-  min,
-  max,
-  required,
-  email,
-  regexp,
-  maskFilled
+    min,
+    max,
+    required,
+    email,
+    regexp,
+    maskFilled
 };
